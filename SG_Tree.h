dee88a2bb572acc02f843d0b5deaf081c529f322
dsambugaro <daniloc@alunos.utfpr.edu.br>
2017-11-15 20:45:23 -0200

    Implementação da função 'tam' e inicio do rebuild da árvore

--- SG_Tree.h
+++ SG_Tree.h
@@ -4,121 +4,147 @@
 #include <math.h>
 
 class SG_Tree{
-  private:
-    No_SG *raiz ;
-    double Alpha; // Parametro que verifica desbalanceamento
-    int qtde ; // Quantidade de itens na arvore
+private:
+	No_SG *raiz ;
+	double Alpha; // Parametro que verifica desbalanceamento
+	int qtde ; // Quantidade de itens na arvore
 
-    double logbx(double base, double value){
-      return (log(value) / log(base)) ;
-    }
-    int logH(int h, double alpha) {
-      return (int)ceil(logbx((1.0/alpha), h));
-    }
+	double logbx(double base, double value){
+		return (log(value) / log(base)) ;
+	}
+	int logH(int h, double alpha) {
+		return (int)ceil(logbx((1.0/alpha), h));
+	}
 
-    void reconstruirArvore(){
-      return ;
-    }
+	int tam(No_SG *r) {
 
-    int inserindo(int v) {
-      if (raiz == NULL) {
-        raiz = new No_SG(v);
-        qtde++;
-        return 0;
-      }
-      No_SG *aux = raiz;
-      bool feito = false;
-      int h = 0;
+		if (r == NULL) {
+			return 0;
+		}
 
-      do {
-        if (v > aux->getValor()) {
-          if (aux->getDir() == NULL) {
-            aux->setDir( new No_SG(v) ); // Insere
-            feito = true;
-          }
-          else {
-            aux = aux->getDir(); // Busca na arvore direita
-          }
-        }
-        else if (v < aux->getValor()) {
-          if (aux->getEsq() == NULL) {
-            aux->setEsq( new No_SG(v) ); // Insere
-            feito = true;
-          }
-          else {
-            aux = aux->getEsq(); // Busca na arvore esquerda
-          }
-        }
-        else {
-          return -1; // Já existe
-        }
-        h++;
-      }while(!feito);
+		int t = 1;
+		t += tam(r->getEsq());
+		t += tam(r->getDir());
+		return t;
+	}
 
-      qtde++;
-      return h;
-    }
+	void reconstruirArvore(No_SG *scapeGoat){
+		return ;
+	}
 
-    void preOrder(No_SG* no){
-      if (no == NULL) return ;
-      std::cout << no->getValor() << " " ;
-      preOrder(no->getEsq());
-      preOrder(no->getDir());
-    }
-    void posOrder(No_SG* no){
-      if (no == NULL) return ;
-      posOrder(no->getEsq());
-      posOrder(no->getDir());
-      std::cout << no->getValor() << " " ;
-    }
-    void inOrder(No_SG* no){
-      if (no == NULL) return ;
-      inOrder(no->getEsq());
-      std::cout << no->getValor() << " " ;
-      inOrder(no->getDir());
-    }
+	int inserindo(int v) {
+		if (raiz == NULL) {
+			raiz = new No_SG(v);
+			qtde++;
+			return 0;
+		}
 
-    void destruir(No_SG* no){
-      if(no == NULL) return ;
-      destruir(no->getEsq());
-      destruir(no->getDir());
-      delete no ;
+		No_SG *aux = raiz;
+		No_SG *inserido = NULL;
+		bool feito = false;
+		int h = 0;
 
-    }
-  protected:
+		do {
+			if (v > aux->getValor()) {
+				if (aux->getDir() == NULL) {
+					aux->setDir( new No_SG(v) ); // Insere
+					inserido = aux->getDir(); // in aponta pro nó inserido
+					feito = true;
+				}
+				else {
+					aux = aux->getDir(); // Busca na arvore direita
+				}
+			}
+			else if (v < aux->getValor()) {
+				if (aux->getEsq() == NULL) {
+					aux->setEsq( new No_SG(v) ); // Insere
+					inserido = aux->getEsq(); // in aponta pro nó inserido
+					feito = true;
+				}
+				else {
+					aux = aux->getEsq(); // Busca na arvore esquerda
+				}
+			}
+			else {
+				return -1; // Já existe
+			}
+			h++;
+		} while(!feito);
 
-  public:
-    SG_Tree(double A = 2.0/3.0) {
-      raiz = NULL;
-      Alpha = A;
-    }
-    ~SG_Tree() {
-      destruir(raiz);
-    }
-    bool inserir(int valor) {
-      int h = inserindo(valor); // h recebe a altura da inserção de v
-      if (h == -1) return false;
-      if (h > logH(qtde, Alpha)) {
-        std::cout << "Desbalanceada!!\n";
-        // Buscar a nova raiz
+		if (h > logH(qtde, Alpha)) {
+			std::cout << "Desbalanceada!!\n";
+			// Buscar a nova raiz
+			while (3 * tam(inserido) <= 2 * tam(inserido->getPai())){
+				inserido = inserido->getPai();
+			}
 
-        // Reconstruir a nova arvore em relação a nova raiz
-        reconstruirArvore();
-      }
+			// Reconstruir a nova arvore em relação a nova raiz
+			reconstruirArvore(aux->getPai());
+		}
 
-      return true;
-    }
-    void remover(int valor) ;
-    void buscar(int valor) ;
-    void preOrder(){
-      preOrder(raiz);
-    }
-    void posOrder(){
-      posOrder(raiz);
-    }
-    void inOrder(){
-      inOrder(raiz);
-    }
+		qtde++;
+		return h;
+	}
+
+	void preOrder(No_SG* no){
+		if (no == NULL) return ;
+		std::cout << no->getValor() << " " ;
+		preOrder(no->getEsq());
+		preOrder(no->getDir());
+	}
+	void posOrder(No_SG* no){
+		if (no == NULL) return ;
+		posOrder(no->getEsq());
+		posOrder(no->getDir());
+		std::cout << no->getValor() << " " ;
+	}
+	void inOrder(No_SG* no){
+		if (no == NULL) return ;
+		inOrder(no->getEsq());
+		std::cout << no->getValor() << " " ;
+		inOrder(no->getDir());
+	}
+
+	void destruir(No_SG* no){
+		if(no == NULL) return ;
+		destruir(no->getEsq());
+		destruir(no->getDir());
+		delete no ;
+
+	}
+protected:
+
+public:
+	SG_Tree(double A = 2.0/3.0) {
+		raiz = NULL;
+		Alpha = A;
+	}
+
+	~SG_Tree() {
+		destruir(raiz);
+	}
+
+	bool inserir(int valor) {
+		int h = inserindo(valor); // h recebe a altura da inserção de v
+		if (h == -1) return false;
+		return true;
+	}
+
+	void remover(int valor);
+
+	void buscar(int valor);
+
+	void preOrder(){
+		preOrder(raiz);
+	}
+
+	void posOrder(){
+		posOrder(raiz);
+	}
+
+	void inOrder(){
+		inOrder(raiz);
+	}
 };
 
 #endif
