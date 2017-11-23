22ca958cc96aaa2cd3c8a4e3b4031dc6e61aa9ac
DANIEL AUGUSTO RODRIGUES FARINA <a1857339@debian9-dacom.dacom>
2017-11-23 17:34:32 -0200

    Projeto para debug criado.

---
+++ APS/No_SG.h
@@ -0,0 +1,35 @@
+#ifndef NO_SG
+#define NO_SG
+
+class No_SG {
+	private:
+		int valor;
+		No_SG *dir, *esq, *pai;
+	protected:
+
+	public:
+		No_SG(){
+			valor = 0;
+			dir = esq = pai = NULL;
+		}
+		
+		No_SG(int v){
+			valor = v;
+			dir = esq = pai = NULL;
+		}
+
+		~No_SG(){
+
+		}
+
+		void setValor(int v) { valor = v; }
+		void setDir(No_SG *d) { dir = d; }
+		void setEsq(No_SG *e) { esq = e; }
+		void setPai(No_SG *p) { pai = p; }
+		int getValor() { return valor; }
+		No_SG *getDir() { return dir; }
+		No_SG *getEsq() { return esq; }
+		No_SG *getPai() { return pai; }
+};
+
+#endif
